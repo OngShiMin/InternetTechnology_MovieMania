37ab33419688ed35fb72fa5e7d6a964488bde8c5
Shi Min Ong <2399631o@student.gla.ac.uk>
2019-02-27 11:20:57 +0000

    media directory

--- movie_mania_team_project/movie_mania_team_project/settings.py
+++ movie_mania_team_project/movie_mania_team_project/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 
@@ -66,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
@@ -120,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
 
 # Variables for moviemania
@@ -136,3 +139,7 @@ LOGIN_REDIRECT_URL = '/moviemania/'
 # The page users are directed to if they are not logged in,
 # and are trying to access pages requiring authentication
 LOGIN_URL = '/accounts/login/'
+
+# Media Files
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
