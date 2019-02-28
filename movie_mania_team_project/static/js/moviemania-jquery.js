b466607fa08c6570d2ca9dfe9c6f3db1057d0776
kathrinPanitz <kathrin.panitz@gmx.de>
2019-02-28 13:45:00 +0100

    Tested JQuery in Login page

---
+++ movie_mania_team_project/static/js/moviemania-jquery.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $("#about-btn").click( function(event) {
+        msgstr = $("#msg").html()
+        msgstr = msgstr + "ooo"
+        $("#msg").html(msgstr)
+        alert("You clicked the button using JQuery!");
+
+    });
+
+    $("p").hover( function() {
+        $(this).css('color', 'red');
+    },
+    function() {
+        $(this).css('color', 'blue');
+    });
+
+
+
+
+});
