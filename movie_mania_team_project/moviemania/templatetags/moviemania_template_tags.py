5f052440990875a42a95fed2887e96ba4a50f8ba
kathrinPanitz <kathrin.panitz@gmx.de>
2019-03-07 20:53:34 +0100

    added a template tag for show_category_list and added it to base.html

--- movie_mania_team_project/moviemania/templatetags/moviemania_template_tags.py
+++ movie_mania_team_project/moviemania/templatetags/moviemania_template_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from moviemania.models import Movie
+from moviemania.models import Category
 
 register = template.Library()
 
@@ -7,6 +8,10 @@ register = template.Library()
 @register.inclusion_tag('moviemania/movies.html')
 def get_movie_list():
     return {'movies': Movie.objects.all(),}
+
+@register.inclusion_tag('moviemania/categories.html')
+def get_category_list():
+    return {'categories': Category.objects.all(),}
 
 
 
