4ae7c14e043b61b0d88c2ad762d2b311c4a04cbe
Janaki Radhakrishnan <janaki26794@gmail.com>
2019-03-12 17:33:07 +0000

    forget password functionality..and made changed in population script

--- movie_mania_team_project/populate_moviemania.py
+++ movie_mania_team_project/populate_moviemania.py
@@ -11,19 +11,19 @@ from moviemania.models import Category, Movie
 
 def populate():
     action_movies = [
-        {"title": "Mission Impossible", "views": 32,"director":"Jack","actor":"Lily"},
-        {"title": "Fantastic Beasts: The Crimes of Grindelwald", "views": 32, "director":"Jack","actor":"Lily"},
-        {"title": "Avengers: Infinity War", "views": 16, "director":"Jack","actor":"Lily"}
+        {"title": "Mission Impossible", "views": 32, "director": "Jack", "actor": "Lily"},
+        {"title": "Fantastic Beasts: The Crimes of Grindelwald", "views": 32, "director": "Jack", "actor": "Lily"},
+        {"title": "Avengers: Infinity War", "views": 16, "director": "Jack", "actor": "Lily"}
     ]
 
     drama_movies = [
-        {"title": "Bohemian Rhapsody", "views": 32, "director":"Jack","actor":"Lily"},
-        {"title": "Titanic", "views": 16, "director":"Jack","actor":"Lily"}
+        {"title": "Bohemian Rhapsody", "views": 32, "director": "Jack", "actor": "Lily"},
+        {"title": "Titanic", "views": 16, "director": "Jack", "actor": "Lily"}
     ]
-    
+
     fantasy_movies = [
-        {"title": "Fantastic Beasts: The Crimes of Grindelwald", "views": 32,"director":"Jack","actor":"Lily"},
-        {"title": "Avengers: Infinity War", "views": 16,"director":"Jack","actor":"Lily"}
+        {"title": "Fantastic Beasts: The Crimes of Grindelwald", "views": 32, "director": "Jack", "actor": "Lily"},
+        {"title": "Avengers: Infinity War", "views": 16, "director": "Jack", "actor": "Lily"}
     ]
 
     cats = {"Action": {"movies": action_movies},
@@ -40,7 +40,7 @@ def populate():
             print("- {0} - {1}".format(str(c), str(m)))
 
 
-def add_movie(cat, title, views=0,):
+def add_movie(cat, title, views=0, ):
     m = Movie.objects.get_or_create(category=cat, title=title)[0]
     m.views = views
     m.save()
