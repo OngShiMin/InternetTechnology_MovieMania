fbbcb226cc47c1c818373b8e3c68b8803cc0a016
Shi Min Ong <2399631o@student.gla.ac.uk>
2019-03-18 22:32:12 +0000

    populate_moviemania.py

--- movie_mania_team_project/populate_moviemania.py
+++ movie_mania_team_project/populate_moviemania.py
@@ -15,19 +15,25 @@ def populate():
          "actor":"Lily", "img": "posters/MissionImpossibleFallout.jpg",
          "netflix_link": "", "amazon_link": "", "preview": "",
          },
-        {"title": "Avengers: Infinity War", "views": 16, "likes": 32, "director":"Jack","actor":"Lily",
-         "content": "Exciting action movie about the Avengers",
-         "netflix_link": "", "amazon_link": "", "preview": "https://www.youtube.com/watch?v=6ZfuNTqbHE8","img": "posters/InfinityWar.jpg"}
+        {"title": "Avengers: Infinity War", "views": 16, "likes": 32, "director":"Anthony Russo, Joe Russo",
+         "actor":"Robert Downey Jr., Chris Hemsworth, Mark Ruffalo, Chris Evans, Scarlett Johansson, Don Cheadle, Benedict Cumberbatch,Tom Holland",
+         "content": "The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.",
+         "netflix_link": "", "amazon_link": "https://www.amazon.co.uk/dp/B07CLZKY26?ref_=imdbref_tt_wbr_aiv&tag=imdbtag_tt_wbr_aiv-21", "preview": "https://www.youtube.com/watch?v=6ZfuNTqbHE8","img": "posters/InfinityWar.jpg"
+         },
+         {"title": "Aquaman", "views": 32, "likes": 16, "content": "Arthur Curry, the human-born heir to the underwater kingdom of Atlantis, goes on a quest to prevent a war between the worlds of ocean and land.", 
+         "director":"James Wan", "actor":"Jason Momoa, Amber Heard, Willem Dafoe", "img": "posters/AquaMan.jpg",
+         "netflix_link": "", "amazon_link": "", "preview": "",
+         },
     ]
 
     drama_movies = [
         {"title": "Bohemian Rhapsody", "views": 32, "likes": 16,
-         "content": "A movie about the exciting history of Queen",
+         "content": "The story of the legendary rock band Queen and lead singer Freddie Mercury, leading up to their famous performance at Live Aid (1985).",
          "netflix_link": "", "amazon_link": "", "preview": "https://www.youtube.com/watch?v=mP0VHJYFOAU",
-         "director": "Jack", "actor": "Lily", "img": "posters/BohemianRhapsody.jpg"},
+         "director": "Bryan Singer", "actor": "Rami Malek, Lucy Boynton, Gwilym Lee", "img": "posters/BohemianRhapsody.jpg"},
         {"title": "Titanic", "views": 16, "likes": 16, "director": "Jack", "actor": "Lily",
          "content": "Tragic love story set on the Titanic",
-         "netflix_link": "", "amazon_link": "", "preview": "", "img": "posters/Titanic.jpg"}
+         "netflix_link": "", "amazon_link": "https://www.amazon.co.uk/dp/B07JP1QM39?ref_=imdbref_tt_wbr_aiv&tag=imdbtag_tt_wbr_aiv-21", "preview": "", "img": "posters/Titanic.jpg"}
     ]
 
     fantasy_movies = [
@@ -40,9 +46,9 @@ def populate():
          "preview": "https://www.youtube.com/watch?v=ViuDsy7yb8M", "director": "David Yates",
          "actor": "Eddie Redmayne, Katherine Waterston, Dan Fogler, Alison Sudol, Ezra Miller",
          "content": "Fantastic Beasts and Where to Find Them is a prequel to the Harry Potter movies. "
-                    "Based on J.K. Rowling's original story, it takes place in 1920s New York City and follows "
-                    "Newt Scamander (Eddie Redmayne), a magizoologist and author of a Hogwarts textbook.",
-                    "img": "posters/FantasticBeasts.jpg"}
+         "Based on J.K. Rowling's original story, it takes place in 1920s New York City and follows "
+         "Newt Scamander (Eddie Redmayne), a magizoologist and author of a Hogwarts textbook.",
+         "img": "posters/FantasticBeasts.jpg"}
         ]
 
 
