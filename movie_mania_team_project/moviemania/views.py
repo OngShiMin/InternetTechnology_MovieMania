e0d2db4bc9e765d6099d53e5c828c23a269d27f1
Janaki Radhakrishnan <janaki26794@gmail.com>
2019-03-20 12:49:35 +0000

    changes in view template for similar searches

--- movie_mania_team_project/moviemania/views.py
+++ movie_mania_team_project/moviemania/views.py
@@ -23,6 +23,14 @@ def index(request):
     response = render(request, 'moviemania/index.html', context_dict)
     return response
 
+def get_suggestion_list(request):
+    category_list = Category.objects.order_by('-name')
+    movie_list = Movie.objects.order_by('-likes')[:10]
+
+    context_dict = {'categories': category_list, 'movies': movie_list}
+
+    response = render(request, 'moviemania/movie.html', context_dict)
+    return response
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
