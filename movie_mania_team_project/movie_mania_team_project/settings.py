2eaf0876fbd65fe52188845ef91126e8f3c15bf0
Janaki Radhakrishnan <46479011+JanakiRK@users.noreply.github.com>
2019-03-21 14:43:17 +0000

    Update settings.py

--- movie_mania_team_project/movie_mania_team_project/settings.py
+++ movie_mania_team_project/movie_mania_team_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'lx2nff6(4$7(n^2e@&bcvn6xlklvy=ud&su%$!^yfs0x^f83lp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['moviemania.pythonanywhere.com','127.0.0.1']
+ALLOWED_HOSTS = ['kathrinpanitz.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
